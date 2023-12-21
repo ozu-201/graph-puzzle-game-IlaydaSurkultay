d7e75575d96e5e176263e419024a7e3e24e25923
Ilayda Surkultay <127659973+IlaydaSurkultay@users.noreply.github.com>
2023-12-21 14:04:12 +0300

    week 2

--- src/main.cpp
+++ src/main.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "main.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main{
+    string turkishline;
+    isstream turkishFile("turkish-dictionary.txt");
+    while(gelline(turkishFile,turkishline)){
+        turkishFile >> turkishline;
+        if(turkishline==3){
+            cout<<turkishline;
+        }
+
+    }
+        turkishFile.close();
+};
